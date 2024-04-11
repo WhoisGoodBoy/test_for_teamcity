9fae4edd7472ecc79df48ba52720c626ec7c5563
WhoisGoodBoy <veolvl@gmail.com>
2024-04-11 20:49:48 +0300

    b

---
+++ test_1.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def test_sum():
+    assert 2+2==4
+
+
+def test_multiply():
+    assert 4*4 == 16
+
+
+def test_minus():
+    assert 4-5==1
+
+
+def test_multyply_wrong():
+    assert 5*5 == 20
+
+
+def test_divide():
+    assert 4/2 == 15
+
+def test_divide2():
+    assert 4/2 == 2
